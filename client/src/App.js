1db09367c5e77644fdda6881934908bef1bc175b
TheMaiEQX <mai.the.09@gmail.com>
2019-04-06 19:21:31 -0500

    Added preloader bp, page transitions bp

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,81 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import { TransitionGroup, Transition } from "react-transition-group";
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import About from './pages/About';
-
-
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Transition } from "react-transition-group";
+import { Home, About, Contact, LoadingScreen } from './pages';
 
 class App extends Component {
   state = {
-    timeout: {
+    loadScreenFinished: false,
+    pageTimeout: {
       enter: 500,
       exit: 500
     }
-  }
-
-  setTimeout = (timeout) => this.setState({ timeout: timeout });
+  }  
 
   render() {
+    // Fake loading screen stuff
+    if (!this.state.loadScreenFinished) setTimeout(() => {
+      this.setState({ loadScreenFinished: true })
+    }, 1000);
     return <>
-      <Router forceRefresh={!'pushState' in window.history}>
-        <div className="links">
-          <Link to="/">Home</Link>
-          <Link to="/pageone">One</Link>
-          <Link to="/pagetwo">Two</Link>
-        </div>
-        <Route render={({ location }) => {
-          const { pathname, key } = location;
-          return (
-            <TransitionGroup>
-              <Transition
-                key={key}
-                appear={true}
-                timeout={this.state.timeout}
-              >
-                <Switch location={location}>
-                  <Route exact path="/" render={() => {                    
-                    return <Home
+      {(this.state.loadScreenFinished) ? <>
+        <Router forceRefresh={!'pushState' in window.history}>
+          <div className="links">
+            <Link to="/">Home</Link>
+            <Link to="/pageone">One</Link>
+            <Link to="/pagetwo">Two</Link>
+          </div>
+          <Route exact path="/" >
+            {({ match }) => {
+              return (
+                <Transition
+                  in={match != null}
+                  timeout={this.state.pageTimeout}
+                  unmountOnExit
+                >
+                  <Home />
+                </Transition>
+              )
+            }}
+          </Route>
+          <Route path="/pageone" >
+            {({ match }) => {
+              return (
+                <Transition
+                  in={match != null}
+                  timeout={this.state.pageTimeout}
+                  unmountOnExit
+                >
+                  <Contact />
+                </Transition>
+              )
+            }}
+          </Route>
+          <Route path="/pagetwo" >
+            {({ match }) => {
+              return (
+                <Transition
+                  in={match != null}
+                  timeout={this.state.pageTimeout}
+                  unmountOnExit
+                >
+                  <About />
+                </Transition>
+              )
+            }}
+          </Route>
+        </Router>
+      </> : ''
+      }
+      < Transition
+        in={!this.state.loadScreenFinished}
+        timeout={this.state.pageTimeout}
+        unmountOnExit
+      >
+        <LoadingScreen />
+      </Transition>
 
-                    />
-                  }} />
-                  <Route exact path="/pageone" render={() => {
-                    return <Contact
-
-                    />
-                  }} />
-                  <Route exact path="/pagetwo" component={About} />
-                </Switch>
-              </Transition>
-            </TransitionGroup>
-          )
-        }} />
-      </Router>
     </>
   }
 }
-
 export default App;
---
+++ client/src/pages/LoadingScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class LoadingScreen extends Component {
+  render() {
+    return (
+      <div>
+          Hi I'm the Loading Screen
+      </div>
+    )
+  }
+}
