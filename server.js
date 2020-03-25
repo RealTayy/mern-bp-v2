a4f7f90c3d4bb3dea10ec3feaa0d048e7ea40040
The Mai <mai.the.09@gmail.com>
2020-03-25 11:26:20 -0500

    update all npm packages

--- server.js
+++ server.js
@@ -3,20 +3,22 @@
 |***************/
 /* GENERAL */
 // Utilities for working with file and directory paths
-const path = require('path');
+import * as path from "path";
 // Load enviroment variables from .env into process.env
-require('dotenv').config()
+import * as dotenv from "dotenv";
+dotenv.config();
+// require('dotenv').config()
 
 /* WEB FRAMEWORKS */
 // lightweight web framework for node server
-const express = require('express');
+const express = require("express");
 // Initialize express under app variable
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 /* BODY PARSERS */
 // node.js body parsing middleware avaiable under req.body
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,19 +29,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 	YELLOW      Client Error Codes
 	CYAN        Redirection Codes
   UNCOLORED   Other Codes         */
-if (process.env.NODE_ENV !== 'production') {
-  const logger = require('morgan');
-  app.use(logger('dev'));
+if (process.env.NODE_ENV !== "production") {
+  const logger = require("morgan");
+  app.use(logger("dev"));
 }
 
 /*****************|
 |* SET UP ROUTES *| 
 |*****************/
 // Setup app to serve static files from React App depending on dev/prod
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, './client', 'build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./client", "build")));
 }
-// app.use(express.static(path.join(__dirname, './client')));
 
 // imports in ./routes/index.js (contains API and VIEW routes);
 const routes = require("./routes");
@@ -49,4 +50,6 @@ app.use(routes);
 /*********************************|
 |* LISTEN FOR CONNECTION ON PORT *| 
 |*********************************/
-app.listen(PORT, () => { console.log(`App listening on PORT: ${PORT}`) });
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
